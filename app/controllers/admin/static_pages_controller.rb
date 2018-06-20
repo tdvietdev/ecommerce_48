cdd115e6a43be1cf155c84a9aa9f88436ae17b6b
Trinh Dinh Viet <tdviet.dev@gmail.com>
2018-06-20 20:17:41 +0700

    add_product

--- app/controllers/admin/static_pages_controller.rb
+++ app/controllers/admin/static_pages_controller.rb
@@ -1,8 +1,2 @@
-<<<<<<< HEAD
 class Admin::StaticPagesController < Admin::AdminController
-=======
-class Admin::StaticPagesController < ApplicationController
-  layout "admin/application"
-  def index; end
->>>>>>> add category
 end
